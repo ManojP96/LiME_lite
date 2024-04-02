0c091815ba54b966c25d127ae92ed57e20926fdb
Manoj <129957999+ManojP96@users.noreply.github.com>
2024-04-02 17:43:58 +0530

    Update 4_Model_Build_and_Performance.py

--- pages/4_Model_Build_and_Performance.py
+++ pages/4_Model_Build_and_Performance.py
@@ -134,7 +134,7 @@ if 'Model_results' not in st.session_state:
 #if st.button('Build Model'):
 if 'iterations' not in st.session_state:
    st.session_state['iterations']=1
-save_path = r"Model"
+save_path = "Model"
 if st.session_state["final_selection"] is not None:
   st.write(f'Total combinations created {format_numbers(len(st.session_state["final_selection"]))}')
 if st.checkbox('Build all iterations'):
